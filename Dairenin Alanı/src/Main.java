9821c2b57dc5e16f70e02ab667bc5520d781c5f5
umtazazi <umutazazi3@gmail.com>
2022-04-16 21:35:31 +0300

    new task added

---
+++ Dairenin Alanı/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double r,a,p;
+        System.out.println("Lütfen yarı çap giriniz: ");
+        r = sc.nextDouble();
+        System.out.println("Lütfen merkez açısı ölçüsünü giriniz: ");
+        a = sc.nextDouble();
+        p = Math.PI;
+        double daireAlanı = (p*(r*r)*a)/(360);
+        System.out.println("Alan : "+daireAlanı);
+
+    }
+}
